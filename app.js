3d816c2aa1560bbfb099474614cc8ce0514fedae
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-09-04 19:23:52 -0700

    almost done for now

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app1.post('/slack', async (req, res) => {
   // For example:
   if (data.action === 'start') {
       console.log('Starting process...');
-      await app.start(process.env.PORT || 3002 );
+      //await app.start(process.env.PORT || 3002 );
       // Code to start a process
   } else if (data.action === 'stop') {
       console.log('Stopping process...');
@@ -51,51 +51,378 @@ app1.post('/slack', async (req, res) => {
   }
 
 
-  if (data.action === 'talk') {
+  if (data.action === 'BA-target') {
     //console.log('Starting process...');
     // Code to start a process
     await app.client.chat.postMessage({
       token: process.env.O_AUTH,
       channel: channelId,
-      "blocks": [
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "radio_buttons",
-					"options": [
-						{
-							"text": {
-								"type": "plain_text",
-								"text": " *run server 1* ",
-								"emoji": true
-							},
-							"value": "value-0"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "*run server 2*",
-								"emoji": true
-							},
-							"value": "value-1"
-						},
-						{
-							"text": {
-								"type": "plain_text",
-								"text": "*Run server 3*",
-								"emoji": true
-							},
-							"value": "value-2"
-						}
-					],
-					"action_id": "actionId-0"
-				}
-			]
-		}
-	]
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "target.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
     });
   }
+
+
+  if (data.action === 'BA-target100') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T4 target100.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-target1010') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T10 target1010.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-target1011') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T11 target1011.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-target1100') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T12 target1100.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-target1101') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T13 target1101.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-target1111') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T15 target1111.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+  if (data.action === 'BA-newtarget011') {
+    await app.client.chat.postMessage({
+      token: process.env.O_AUTH,
+      channel: channelId,
+      text: 'Server Uptime Alert',
+      blocks: [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "Server Uptime Alert",
+            "emoji": true
+          }
+        },
+        {
+          "type": "rich_text",
+          "elements": [
+            {
+              "type": "rich_text_section",
+              "elements": [
+                {
+                  "type": "text",
+                  "text": "T3 newtarget011.brightarrow.com/r/ ",
+                  "style": {
+                    "bold": true
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "  is not properly responding!"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "<@U06JXTM3QSY>"
+          }
+        }
+      ]
+    });
+  }
+
+
 
 
 
